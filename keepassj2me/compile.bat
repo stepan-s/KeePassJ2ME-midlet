8949900898248230aacfe169da23476868d4f29a
Naomaru Itoi <naomaru@users.sourceforge.net>
2007-09-21 19:11:12 +0000

    fix MANIFEST.MF to support Motorola RAZR 3
    git-svn-id: https://keepassj2me.svn.sourceforge.net/svnroot/keepassj2me/trunk@72 37bc80f5-c714-4e7e-901a-299624aa0b57

--- keepassj2me/compile.bat
+++ keepassj2me/compile.bat
@@ -12,9 +12,11 @@ rm -rf %INSTALL_DIR%
 mkdir %INSTALL_DIR%
 javac -d class src/org/phoneid/keepassinstaller/*.java
 jar cvf %INSTALL_DIR%/KeePassInstaller.jar -C class org
-cp -r doc/README.txt KeePassInstaller.bat res/images %WTK_HOME%/apps/KeePassJ2ME/bin/KeePassJ2ME.jar %INSTALL_DIR%
+cp -r doc/README.txt KeePassInstaller.bat res/images %WTK_HOME%/apps/KeePassJ2ME/bin/KeePassJ2ME.jar %WTK_HOME%/apps/KeePassJ2ME/bin/KeePassJ2ME.jad %INSTALL_DIR%
 cd %INSTALL_DIR%
 jar xvf KeePassJ2ME.jar
+echo please get rid of carriage return in %INSTALL_DIR%/META-INF/MANIFEST.MF
+pause
 rm KeePassJ2ME.jar
 cd ..
 zip -r %INSTALL_DIR%.zip %INSTALL_DIR%
