9957fb1b0d145ca7848239576c40c1f9b964700b
Stepan Strelets <stepan_s@users.sourceforge.net>
2010-03-19 19:15:07 +0000

    Added some tests
    git-svn-id: https://keepassj2me.svn.sourceforge.net/svnroot/keepassj2me/keepassj2me/trunk@197 37bc80f5-c714-4e7e-901a-299624aa0b57

--- src-test/net/sourceforge/keepassj2me/tests/KeydbTest.java
+++ src-test/net/sourceforge/keepassj2me/tests/KeydbTest.java
@@ -1,15 +1,22 @@
 package net.sourceforge.keepassj2me.tests;
 
 import net.sourceforge.keepassj2me.keydb.KeydbDatabase;
+import net.sourceforge.keepassj2me.keydb.KeydbEntry;
 import net.sourceforge.keepassj2me.keydb.KeydbException;
 import net.sourceforge.keepassj2me.keydb.KeydbGroup;
 import net.sourceforge.keepassj2me.keydb.KeydbLockedException;
+import net.sourceforge.keepassj2me.keydb.KeydbUtil;
 import j2meunit.framework.Test;
 import j2meunit.framework.TestCase;
 import j2meunit.framework.TestMethod;
 import j2meunit.framework.TestSuite;
 
 public class KeydbTest extends TestCase {
+	private String pass = "pass";
+	private byte[] keyfile = new byte[] {1,2,3,4,5,6,7,8,9,10};
+	private int rounds = 100;
+	private KeydbDatabase db;
+	
 	public KeydbTest()
 	{
 	}
@@ -24,6 +31,11 @@ public class KeydbTest extends TestCase {
 		TestSuite aSuite = new TestSuite();
 		
 		aSuite.addTest(new KeydbTest("testDb", new TestMethod() { public void run(TestCase tc) {((KeydbTest) tc).testDb(); } }));
+		aSuite.addTest(new KeydbTest("testGroups", new TestMethod() { public void run(TestCase tc) {((KeydbTest) tc).testGroups(); } }));
+		aSuite.addTest(new KeydbTest("testGroupsDeleteLast", new TestMethod() { public void run(TestCase tc) {((KeydbTest) tc).testGroupsDeleteLast(); } }));
+		aSuite.addTest(new KeydbTest("testGroupsDeleteMiddle", new TestMethod() { public void run(TestCase tc) {((KeydbTest) tc).testGroupsDeleteMiddle(); } }));
+		aSuite.addTest(new KeydbTest("testGroupsDeleteFirst", new TestMethod() { public void run(TestCase tc) {((KeydbTest) tc).testGroupsDeleteFirst(); } }));
+		aSuite.addTest(new KeydbTest("testEntries", new TestMethod() { public void run(TestCase tc) {((KeydbTest) tc).testEntries(); } }));
 
 		return aSuite;
 	}
@@ -35,38 +47,26 @@ public class KeydbTest extends TestCase {
 		System.out.println("--------------------------------------------");
 	}
 	
-	public void testDb()
-	{
-		String pass = "pass";
-		byte[] keyfile = new byte[] {1,2,3,4,5,6,7,8,9,10};
-		int count = 100;
-
+	/* PRIMITIVES */
+	
+	private void createDb() {
 		out("CREATE DB");
-		KeydbDatabase db = new KeydbDatabase();
+		db = new KeydbDatabase();
 		assertNotNull("Database", db);
 		try {
-			db.create(pass, keyfile, 1000);
+			db.create(pass, keyfile, rounds);
 		} catch (KeydbException e) {
 			assertTrue("create", false);
 		}
-		
-		out("CREATE GROUPS");
-		int[] ids = new int[count];
-		for(int i = 0; i < count; ++i) {
-			KeydbGroup gr = new KeydbGroup(db);
-			gr.parentId = 0;
-			gr.imageIndex = i & 0xF;
-			gr.level = 0;
-			gr.name = "group"+i;
-			gr.save();
-			ids[i] = gr.id;
-		}
-		assertTrue("change", db.isChanged());
-		
+	}
+	
+	private void lockDb() {
 		out("LOCK");
 		db.lock();
 		assertTrue("lock", db.isLocked());
-		
+	}
+	
+	private void unlockDb() {
 		out("UNLOCK");
 		try {
 			db.unlock(pass, keyfile);
@@ -74,25 +74,113 @@ public class KeydbTest extends TestCase {
 			assertTrue("unlock", false);
 		}
 		assertTrue("unlock", !db.isLocked());
+	}
+	
+	private String getGroupHash(KeydbGroup gr) {
+		return KeydbUtil.hashToString(KeydbUtil.hash("group:"+gr.id+":"+gr.parentId+":"+gr.level+":"+gr.imageIndex));
+	}
+	private KeydbGroup createGroup(int gid, int level) {
+		KeydbGroup gr = new KeydbGroup(db);
+		try {
+			gr.id = db.getUniqueGroupId();
+		} catch (KeydbLockedException e) {
+			assertTrue("getUniqueGroupId", false);
+		}
+		gr.parentId = gid;
+		gr.level = level;
+		gr.imageIndex = gr.id & 0x3F;
+		gr.name = getGroupHash(gr);
+		return gr;
+	}
+	private void checkGroup(KeydbGroup gr) {
+		assertEquals(gr.name, getGroupHash(gr));
+	}
+
+	private String getEntryHash(KeydbEntry en) {
+		return KeydbUtil.hashToString(KeydbUtil.hash(
+				"entry:"+KeydbUtil.hashToString(KeydbUtil.hash(en.getUUID()))
+				+":"+en.groupId
+				+":"+en.imageIndex
+				+":"+en.getNote()
+				+":"+en.getPassword()
+				+":"+en.getUrl()
+				+":"+en.getUsername()
+				+":"+KeydbUtil.hashToString(en.getBinaryData())
+				+":"+en.getBinaryDesc()
+			));
+	}
+	private void changeEntryData(KeydbEntry en) {
+		byte[] uuid = en.createUUID();
+		en.setUUID(uuid);
+		en.imageIndex = uuid[0] & 0x3F;
+		en.setNote(KeydbUtil.hashToString(KeydbUtil.hash("note:"+uuid[1]+":"+uuid[2])));
+		en.setPassword(KeydbUtil.hashToString(KeydbUtil.hash("pass:"+uuid[3]+":"+uuid[4])));
+		en.setUrl(KeydbUtil.hashToString(KeydbUtil.hash("url:"+uuid[5]+":"+uuid[6])));
+		en.setUsername(KeydbUtil.hashToString(KeydbUtil.hash("user:"+uuid[7]+":"+uuid[8])));
+		en.setBinaryData(KeydbUtil.hash("data:"+uuid[8]+":"+uuid[9]));
+		en.setBinaryDesc(KeydbUtil.hashToString(KeydbUtil.hash("desc:"+uuid[10]+":"+uuid[11])));
+		en.title = getEntryHash(en);
+	}
+	private KeydbEntry createEntry(int gid) {
+		KeydbEntry en = new KeydbEntry(db);
+		en.groupId = gid;
+		changeEntryData(en);
+		return en;
+	}
+	private void checkEntry(KeydbEntry en) {
+		assertEquals(en.title, getEntryHash(en));
+	}
+	
+	/* TESTS */
+
+	/**
+	 * Test db creation, locking and unlocking
+	 */
+	public void testDb() {
+		this.createDb();
+		KeydbGroup gr = new KeydbGroup(db);
+		gr.save();
+		this.lockDb();
+		this.unlockDb();
+		db.close();
+	}
+	
+	/**
+	 * Check massive group creation and deletion 
+	 */
+	public void testGroups() {
+		this.createDb();
+		
+		int groups_count = 20;//100
+
+		out("CREATE GROUPS");
+		int[] ids = new int[groups_count];
+		for(int i = 0; i < groups_count; ++i) {
+			KeydbGroup gr = createGroup(0, 0);
+			gr.save();
+			ids[i] = gr.id;
+		}
+		assertTrue("change", db.isChanged());
+		
+		this.lockDb();
+		this.unlockDb();
+		
+		assertEquals(groups_count, db.getHeader().getGroupsCount());
 		
 		out("CHECK GROUPS");
-		for(int i = 0; i < count; ++i) {
+		for(int i = 0; i < groups_count; ++i) {
 			KeydbGroup gr = null;
 			try {
 				gr = db.getGroup(ids[i]);
 			} catch (KeydbException e) {
 				assertTrue("getGroup", false);
 			}
-			
 			assertNotNull("group", gr);
-			assertEquals("group"+i, gr.name);
-			assertEquals(i & 0xF, gr.imageIndex);
-			assertEquals(0, gr.parentId);
-			assertEquals(0, gr.level);
+			checkGroup(gr);
 		}
 		
 		out("REMOVE GROUPS");
-		for(int i = 0; i < count; i += 2) {
+		for(int i = 0; i < groups_count; i += 2) {
 			KeydbGroup gr = null;
 			try {
 				gr = db.getGroup(ids[i]);
@@ -107,7 +195,7 @@ public class KeydbTest extends TestCase {
 		}
 		
 		out("CHECK GROUPS");
-		for(int j = 0; j < count; j += 2) {
+		for(int j = 0; j < groups_count; j += 2) {
 			int i = j + 1;
 			KeydbGroup gr = null;
 			try {
@@ -115,12 +203,246 @@ public class KeydbTest extends TestCase {
 			} catch (KeydbException e) {
 				assertTrue("getGroup", false);
 			}
-			
 			assertNotNull("group", gr);
-			assertEquals("group"+i, gr.name);
-			assertEquals(i & 0xF, gr.imageIndex);
-			assertEquals(0, gr.parentId);
-			assertEquals(0, gr.level);
+			checkGroup(gr);
 		}
+		
+		db.close();
+	}
+	
+	/**
+	 * Check group deletion: last
+	 */
+	public void testGroupsDeleteLast() {
+		this.createDb();
+		
+		out("CREATE GROUPS");
+		
+		KeydbGroup gr;
+		
+		gr = createGroup(0, 0);
+		gr.save();
+		int id1 = gr.id;
+
+		gr = createGroup(0, 0);
+		gr.save();
+		int id2 = gr.id;
+
+		gr = createGroup(0, 0);
+		gr.save();
+		int id3 = gr.id;
+		
+		out("REMOVE GROUPS");
+		try {
+			gr = db.getGroup(id3);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		try {
+			db.deleteGroup(gr.index);
+		} catch (KeydbLockedException e) {
+			assertTrue("delete group", false);
+		}
+		
+		out("CHECK GROUPS");
+		try {
+			gr = db.getGroup(id1);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		assertNotNull("group", gr);
+		checkGroup(gr);
+		
+		try {
+			gr = db.getGroup(id2);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		assertNotNull("group", gr);
+		checkGroup(gr);
+		
+		db.close();
+	}
+
+	/**
+	 * Check group deletion: middle
+	 */
+	public void testGroupsDeleteMiddle() {
+		this.createDb();
+		
+		out("CREATE GROUPS");
+		
+		KeydbGroup gr;
+		
+		gr = createGroup(0, 0);
+		gr.save();
+		int id1 = gr.id;
+
+		gr = createGroup(0, 0);
+		gr.save();
+		int id2 = gr.id;
+
+		gr = createGroup(0, 0);
+		gr.save();
+		int id3 = gr.id;
+		
+		out("REMOVE GROUPS");
+		try {
+			gr = db.getGroup(id2);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		try {
+			db.deleteGroup(gr.index);
+		} catch (KeydbLockedException e) {
+			assertTrue("delete group", false);
+		}
+		
+		out("CHECK GROUPS");
+		try {
+			gr = db.getGroup(id1);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		assertNotNull("group", gr);
+		checkGroup(gr);
+		
+		try {
+			gr = db.getGroup(id3);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		assertNotNull("group", gr);
+		checkGroup(gr);
+		
+		db.close();
+	}
+	
+	/**
+	 * Check group deletion: first
+	 */
+	public void testGroupsDeleteFirst() {
+		this.createDb();
+		
+		out("CREATE GROUPS");
+		
+		KeydbGroup gr;
+		
+		gr = createGroup(0, 0);
+		gr.save();
+		int id1 = gr.id;
+
+		gr = createGroup(0, 0);
+		gr.save();
+		int id2 = gr.id;
+
+		gr = createGroup(0, 0);
+		gr.save();
+		int id3 = gr.id;
+		
+		out("REMOVE GROUPS");
+		try {
+			gr = db.getGroup(id1);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		try {
+			db.deleteGroup(gr.index);
+		} catch (KeydbLockedException e) {
+			assertTrue("delete group", false);
+		}
+		
+		out("CHECK GROUPS");
+		try {
+			gr = db.getGroup(id2);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		assertNotNull("group", gr);
+		checkGroup(gr);
+		
+		try {
+			gr = db.getGroup(id3);
+		} catch (KeydbException e) {
+			assertTrue("getGroup", false);
+		}
+		assertNotNull("group", gr);
+		checkGroup(gr);
+		
+		db.close();
+	}
+
+	/**
+	 * Check massive entries creation and deletion 
+	 */
+	public void testEntries() {
+		this.createDb();
+		
+		out("CREATE GROUP");
+		KeydbGroup gr;
+		gr = createGroup(0, 0);
+		gr.save();
+		int id1 = gr.id;
+		
+		int entries_count = 20;//100
+
+		out("CREATE ENTRIES");
+		for(int i = 0; i < entries_count; ++i) {
+			KeydbEntry en = createEntry(id1);
+			en.save();
+		}
+		assertTrue("change", db.isChanged());
+		
+		this.lockDb();
+		this.unlockDb();
+		
+		assertEquals(entries_count, db.getHeader().getEntriesCount());
+		
+		out("CHECK ENTRIES");
+		for(int i = 0; i < entries_count; ++i) {
+			KeydbEntry en = null;
+			try {
+				en = db.getEntryByIndex(id1, i);
+			} catch (KeydbException e) {
+				assertTrue("getEntry", false);
+			}
+			assertNotNull("entry", en);
+			checkEntry(en);
+		}
+		
+		out("REMOVE ENTRIES");
+		for(int i = 0; i < entries_count / 2; ++i) {
+			try {
+				db.deleteEntry(i);
+			} catch (KeydbLockedException e) {
+				assertTrue("delete entry", false);
+			}
+		}
+		
+		assertEquals(entries_count / 2, db.getHeader().getEntriesCount());
+		
+		out("CHECK ENTRIES");
+		for(int i = 0; i < entries_count / 2; ++i) {
+			KeydbEntry en = null;
+			try {
+				en = db.getEntryByIndex(id1, 1);
+			} catch (KeydbException e) {
+				assertTrue("getEntry", false);
+			}
+			assertNotNull("entry", en);
+			checkEntry(en);
+		}
+		
+		out("DELETE GROUP");
+		try {
+			db.deleteGroup(0);
+		} catch (KeydbLockedException e) {
+			assertTrue("delete group", false);
+		}
+		
+		assertEquals(0, db.getHeader().getEntriesCount());
+		assertEquals(0, db.getHeader().getGroupsCount());
+		
+		db.close();
 	}
 }
